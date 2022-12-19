91e3bad54769b472818991cf02104ec7cb4ee64c
polymorpher <aaron@qokka.ai>
2022-12-18 17:18:35 -0800

    refactor api

--- routes/index.js
+++ routes/index.js
@@ -5,26 +5,10 @@ const { Logger } = require('../logger')
 const { body, validationResult } = require('express-validator')
 const rateLimit = require('express-rate-limit')
 const appConfig = require('../config')
-const axios = require('axios')
-const { XMLParser } = require('fast-xml-parser')
 const { getDomainRegistrationEvent } = require('../src/w3utils')
 const { v1: uuid } = require('uuid')
 const { Purchase } = require('../src/data/purchase')
-const base = axios.create({
-  baseURL: appConfig.enom.test ? 'https://resellertest.enom.com' : 'https://reseller.enom.com'
-})
-
-base.interceptors.request.use((config) => {
-  // use config.params if it has been set
-  config.params = config.params || {}
-  // add any client instance specific params to config
-  config.params.UID = appConfig.enom.uid
-  config.params.PW = appConfig.enom.pw
-  config.params.TLD = appConfig.tld
-  config.params.responseType = 'xml'
-  config.params.version = '2'
-  return config
-})
+const domainApiProvider = require('../src/enom-api')
 
 const limiter = (args) => rateLimit({
   windowMs: 1000 * 60,
@@ -38,83 +22,6 @@ router.get('/health', async (req, res) => {
   res.send('OK').end()
 })
 
-const checkIsDomainAvailable = async ({ sld }) => {
-  const { data } = await base.get('/interface.asp', {
-    params: {
-      Command: 'check',
-      SLD: sld,
-      includeprice: '1',
-      includeproperties: '1',
-      includeeap: '1'
-    }
-  })
-  const parser = new XMLParser()
-  const parsed = parser.parse(data)['interface-response']
-  console.log('[check-domain]', sld, JSON.stringify(parsed))
-  const {
-    Domains: {
-      Domain: {
-        RRPCode: responseCode,
-        RRPText: responseText,
-        IsPremium: isPremium,
-        IsPlatinum: isPlatinum,
-        IsEAP: isEap,
-        Prices: {
-          Registration: regPrice,
-          Renewal: renewPrice,
-          Transfer: transferPrice,
-          Restore: restorePrice
-        }
-      }
-    }
-  } = parsed
-  const isRegistered = parseInt(responseCode) !== 210
-  const isReserved = (isPremium.toLowerCase() !== 'false' ||
-    isPlatinum.toLowerCase() !== 'false' ||
-    isEap.toLowerCase() !== 'false')
-  const isAvailable = !isRegistered && !isReserved && regPrice < 50
-  return { isAvailable, isReserved, isRegistered, regPrice, renewPrice, transferPrice, restorePrice, responseText }
-}
-
-const purchaseDomain = async ({ sld }) => {
-  const r = appConfig.enom.defaultRegistrant
-  const { data } = await base.get('/interface.asp', {
-    params: {
-      Command: 'purchase',
-      SLD: sld,
-      NS1: appConfig.enom.ns1,
-      NS2: appConfig.enom.ns2,
-      RegistrantFirstName: r.RegistrantFirstName,
-      RegistrantLastName: r.RegistrantLastName,
-      RegistrantAddress1: r.RegistrantAddress1,
-      RegistrantCity: r.RegistrantCity,
-      RegistrantStateProvince: r.RegistrantStateProvince,
-      RegistrantPostalCode: r.RegistrantPostalCode,
-      RegistrantCountry: r.RegistrantCountry,
-      RegistrantEmailAddress: r.RegistrantEmailAddress,
-      RegistrantPhone: r.RegistrantPhone,
-      RegistrantFax: r.RegistrantFax,
-    }
-  })
-  const parser = new XMLParser()
-  const parsed = parser.parse(data)['interface-response']
-  console.log('[purchase-domain]', sld, JSON.stringify(parsed))
-  const {
-    TotalCharged: pricePaid,
-    OrderID: orderId,
-    DomainInfo: {
-      RegistryCreateDate: domainCreationDate,
-      RegistryExpDate: domainExpiryDate,
-    },
-    RRPCode: responseCode,
-    RRPText: responseText,
-    TrackingKey: traceId,
-    RequestDateTime: reqTime,
-  } = parsed
-  const success = responseCode === 200
-  return { success, pricePaid, orderId, domainCreationDate, domainExpiryDate, responseCode, responseText, traceId, reqTime }
-}
-
 router.post('/check-domain', limiter(), async (req, res) => {
   const { sld } = req.body
   if (!sld) {
@@ -122,7 +29,7 @@ router.post('/check-domain', limiter(), async (req, res) => {
   }
   try {
     const { isAvailable, isReserved, isRegistered, regPrice, renewPrice, transferPrice, restorePrice, responseText } =
-      await checkIsDomainAvailable({ sld })
+      await domainApiProvider.checkIsDomainAvailable({ sld })
     res.json({ isAvailable, isReserved, isRegistered, regPrice, renewPrice, transferPrice, restorePrice, responseText })
   } catch (ex) {
     console.error('[/check-domain]', { sld })
@@ -150,7 +57,7 @@ router.post('/purchase',
       if (!event) {
         return res.status(StatusCodes.NOT_FOUND).json({ error: 'did not find registration event in txHash', txHash })
       }
-      const { name, owner, baseCost, expires } = event
+      const { name, owner, expires } = event
       if (owner.toLowerCase() !== address.toLowerCase()) {
         return res.status(StatusCodes.BAD_REQUEST).json({
           error: 'registration event owner mismatch',
@@ -180,12 +87,12 @@ router.post('/purchase',
         })
       }
       purchasePending[domain] = rid
-      const { isAvailable, ...checkResponseArgs } = await checkIsDomainAvailable({ sld: name })
+      const { isAvailable, ...checkResponseArgs } = await domainApiProvider.checkIsDomainAvailable({ sld: name })
       if (!isAvailable) {
         return res.status(StatusCodes.BAD_REQUEST).json({ error: 'domain not available', ...checkResponseArgs })
       }
 
-      const { success, pricePaid, orderId, domainCreationDate, domainExpiryDate, responseCode, responseText, traceId, reqTime } = await purchaseDomain({ sld: name })
+      const { success, pricePaid, orderId, domainCreationDate, domainExpiryDate, responseCode, responseText, traceId, reqTime } = await domainApiProvider.purchaseDomain({ sld: name })
       if (!success) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'purchase failed', domain: name, responseText })
       }
---
+++ src/enom-api.js
@@ -0,0 +1,98 @@
+const axios = require('axios')
+const appConfig = require('../config')
+const { XMLParser } = require('fast-xml-parser')
+
+const base = axios.create({
+  baseURL: appConfig.enom.test ? 'https://resellertest.enom.com' : 'https://reseller.enom.com'
+})
+
+base.interceptors.request.use((config) => {
+  // use config.params if it has been set
+  config.params = config.params || {}
+  // add any client instance specific params to config
+  config.params.UID = appConfig.enom.uid
+  config.params.PW = appConfig.enom.pw
+  config.params.TLD = appConfig.tld
+  config.params.responseType = 'xml'
+  config.params.version = '2'
+  return config
+})
+
+const checkIsDomainAvailable = async ({ sld }) => {
+  const { data } = await base.get('/interface.asp', {
+    params: {
+      Command: 'check',
+      SLD: sld,
+      includeprice: '1',
+      includeproperties: '1',
+      includeeap: '1'
+    }
+  })
+  const parser = new XMLParser()
+  const parsed = parser.parse(data)['interface-response']
+  console.log('[check-domain]', sld, JSON.stringify(parsed))
+  const {
+    Domains: {
+      Domain: {
+        RRPCode: responseCode,
+        RRPText: responseText,
+        IsPremium: isPremium,
+        IsPlatinum: isPlatinum,
+        IsEAP: isEap,
+        Prices: {
+          Registration: regPrice,
+          Renewal: renewPrice,
+          Transfer: transferPrice,
+          Restore: restorePrice
+        }
+      }
+    }
+  } = parsed
+  const isRegistered = parseInt(responseCode) !== 210
+  const isReserved = (isPremium.toLowerCase() !== 'false' ||
+    isPlatinum.toLowerCase() !== 'false' ||
+    isEap.toLowerCase() !== 'false')
+  const isAvailable = !isRegistered && !isReserved && regPrice < 50
+  return { isAvailable, isReserved, isRegistered, regPrice, renewPrice, transferPrice, restorePrice, responseText }
+}
+
+const purchaseDomain = async ({ sld }) => {
+  const r = appConfig.enom.defaultRegistrant
+  const { data } = await base.get('/interface.asp', {
+    params: {
+      Command: 'purchase',
+      SLD: sld,
+      NS1: appConfig.enom.ns1,
+      NS2: appConfig.enom.ns2,
+      RegistrantFirstName: r.RegistrantFirstName,
+      RegistrantLastName: r.RegistrantLastName,
+      RegistrantAddress1: r.RegistrantAddress1,
+      RegistrantCity: r.RegistrantCity,
+      RegistrantStateProvince: r.RegistrantStateProvince,
+      RegistrantPostalCode: r.RegistrantPostalCode,
+      RegistrantCountry: r.RegistrantCountry,
+      RegistrantEmailAddress: r.RegistrantEmailAddress,
+      RegistrantPhone: r.RegistrantPhone,
+      RegistrantFax: r.RegistrantFax,
+    }
+  })
+  const parser = new XMLParser()
+  const parsed = parser.parse(data)['interface-response']
+  console.log('[purchase-domain]', sld, JSON.stringify(parsed))
+  const {
+    TotalCharged: pricePaid,
+    OrderID: orderId,
+    DomainInfo: {
+      RegistryCreateDate: domainCreationDate,
+      RegistryExpDate: domainExpiryDate,
+    },
+    RRPCode: responseCode,
+    RRPText: responseText,
+    TrackingKey: traceId,
+    RequestDateTime: reqTime,
+  } = parsed
+  const success = responseCode === 200
+  return { success, pricePaid, orderId, domainCreationDate, domainExpiryDate, responseCode, responseText, traceId, reqTime }
+}
+
+module.exports = { checkIsDomainAvailable, purchaseDomain }

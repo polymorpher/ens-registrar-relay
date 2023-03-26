38e3bf0b4084a1375d3da7c88c89937cd4133dd9
polymorpher <aaron@qokka.ai>
2023-03-26 15:31:29 -0700

    fix bugs

--- deploy/enable.sh
+++ deploy/enable.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo cp /1ns-registrar-relay.service /etc/systemd/system/1ns-registrar-relay.service
+sudo cp 1ns-registrar-relay.service /etc/systemd/system/1ns-registrar-relay.service
 sudo systemctl enable 1ns-registrar-relay
 sudo systemctl start 1ns-registrar-relay
-systemctl status onewallet-relayer
+systemctl status 1ns-registrar-relay

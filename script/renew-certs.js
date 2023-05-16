b2faae45fa644262d1eedba68697c425d69c8745
polymorpher <aaron@qokka.ai>
2023-05-15 20:33:20 -0700

    renew-certs

--- script/renew-certs.js
+++ script/renew-certs.js
@@ -1,7 +1,32 @@
 const gcp = require('../src/gcp-certs')
 const le = require('../src/letsencrypt-certs')
+const { nameExpires } = require('../src/w3utils')
+const getDomainFromDNSNames = ({ dnsNames }) => {
+  const t = dnsNames.filter(d => !d.includes('*'))
+  if (t.length >= 1) {
+    return t[0]
+  }
+  const parts = dnsNames[0].split('.').filter(p => p !== '*')
+  return parts.join('.')
+}
+
 async function main () {
-  const certs = gcp.listCertificates()
+  const certs = await gcp.listCertificates()
+  const now = Date.now()
+  for (const c of certs) {
+    const domain = getDomainFromDNSNames(c.sanDnsnames)
+    const certExpires = Number(c.expireTime.seconds) * 1000
+    const sld = domain.split('.')[0]
+    const expires = await nameExpires(sld)
+    if (expires > now && expires > certExpires + 3600 * 24 * 1000 * 7) {
+      // should renew cert
+      const { certId, certMapId } = await le.renewCertificate({ sld })
+      console.log(`Renewed ${sld} (expiry: ${new Date(expires).toLocaleString()}); certId=${certId} certMapId=${certMapId}`)
+    } else {
+      // skip and log
+      console.log(`Skipped renewing for ${sld}; (expiry: ${new Date(expires).toLocaleString()}) certExpiry: ${new Date(certExpires).toLocaleString()}`)
+    }
+  }
 }
 
 main().catch(ex => console.error(ex))

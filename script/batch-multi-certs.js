36e4ade747b88e1b6d86d4716bd4f66f4e22c5ab
polymorpher <aaron@qokka.ai>
2024-01-28 02:18:21 -0800

    docs and scripts

--- script/batch-multi-certs.js
+++ script/batch-multi-certs.js
@@ -6,6 +6,16 @@ const dig = require('node-dig-dns')
 const config = require('../config')
 const chars = []
 
+const CertIdPrefix = process.env.CERT_ID_PREFIX ?? `batch-cert-${new Date().toISOString().slice(0, 10).replaceAll('-', '')}`
+const ValidARecord = config.dns.ip ?? '34.160.72.19'
+const Excluded = process.env.EXCLUDED_DOMAINS
+  ? JSON.parse(process.env.EXCLUDED_DOMAINS)
+  : ['li', 'ml', 'ba', 'ec', 'au', 'ep', 'eu', 'un',
+      '0', '00', '01', '02', '03', '04',
+      'h', '0', '1', 's'
+    ]
+const GenerateWildcard = process.env.GENERATE_WILDCARD === '1' || process.env.GENERATE_WILDCARD === 'true'
+
 async function batchGenerate ({ slds, id }) {
   console.log(`generating certs for ${slds.length} slds: ${JSON.stringify(slds)}`)
   // const finalSlds = []
@@ -23,7 +33,7 @@ async function batchGenerate ({ slds, id }) {
   const finalSlds = []
   for (const chunk of lodash.chunk(filteredSlds, 50)) {
     const answers = await Promise.all(chunk.map(sld => dig([`${sld}.${config.tld}`, 'A'])))
-    const filteredChunk = answers.filter(e => e?.answer?.[0]?.value === '34.160.72.19')
+    const filteredChunk = answers.filter(e => e?.answer?.[0]?.value === ValidARecord)
       .map(e => e.answer[0].domain.split('.')[0])
     const badChunk = lodash.difference(chunk, filteredChunk)
     badDomains.push(...badChunk)
@@ -37,7 +47,7 @@ async function batchGenerate ({ slds, id }) {
     const sortedSlds = sldChunk.sort()
     console.log(`Starting chunk ${i} for ${JSON.stringify(sortedSlds)}`)
     const batchId = `${id}-${sortedSlds[0]}-${sortedSlds[sortedSlds.length - 1]}`
-    const ret = await le.createNewMultiCertificate({ id: batchId, slds: sortedSlds, mapEntryWaitPeriod: 60, skipInitDns: true, wc: false })
+    const ret = await le.createNewMultiCertificate({ id: batchId, slds: sortedSlds, mapEntryWaitPeriod: 60, skipInitDns: true, wc: GenerateWildcard })
     console.log(`Finished chunk ${i}`)
     console.log(`Results: ${JSON.stringify(ret.results)}`)
     console.log('Sleeping for 60 seconds')
@@ -45,10 +55,6 @@ async function batchGenerate ({ slds, id }) {
   }
 }
 
-const Excluded = ['li', 'ml', 'ba', 'ec', 'au', 'ep', 'eu', 'un',
-  '0', '00', '01', '02', '03', '04',
-  'h', '0', '1', 's'
-]
 async function main () {
   for (let i = 97; i <= 122; i += 1) {
     chars.push(String.fromCharCode(i))
@@ -60,7 +66,7 @@ async function main () {
   const filtered2chars = all2chars.filter(e => !Excluded.includes(e))
   const filtered1char = chars.filter(e => !Excluded.includes(e))
   // await batchGenerate({ slds: filtered2chars, id: 'all-2-chars-20230119' })
-  await batchGenerate({ slds: [...filtered1char, ...filtered2chars], id: 'all-1-char-20230120' })
+  await batchGenerate({ slds: [...filtered1char, ...filtered2chars], id: CertIdPrefix })
 }
 
 main().catch(console.error)
---
+++ script/batch-multi-certs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /opt/ens-registrar-relay
+node script/batch-multi-certs.js
